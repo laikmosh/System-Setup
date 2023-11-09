ce48821ff104b01c5c57a61d45012ea5c0877914
Pancho Wa <laikmosh@gmail.com>
2023-11-08 18:22:57 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,21 +2,21 @@
 
 # Prompt for sudo password, as it will be required for all the sudo commands
 echo 'Password required for installation:'
-if ! sudo -v; 
-    then    
+if ! sudo -v; then    
     echo "Failed to obtain sudo privileges. Exiting."    
     exit 1
 fi
 
-# Keep sudo credentials alive, so it doesnt keep asking for your password
-while true; do    
-    sudo -n true    
-    sleep 60    
-    kill -0 "$$" || exit
-done 2>/dev/null &
+# # Keep sudo credentials alive, so it doesnt keep asking for your password
+# while true; do    
+#     sudo -n true    
+#     sleep 60    
+#     kill -0 "$$" || exit
+# done 2>/dev/null &
 
-# Setup a temporary folder to download the repository
-mkdir -p "$HOME/SystemSetupTmp"cd "$HOME/SystemSetupTmp" || exit
+# Setup a temporary folder to download the repository, exit script if filesystem is not writable
+mkdir -p "$HOME/SystemSetupTmp"
+cd "$HOME/SystemSetupTmp" || exit
 
 # Download and unzip repository, exit script if download failed
 curl -L -o master.zip https://github.com/laikmosh/System-Setup/archive/refs/heads/main.zip
